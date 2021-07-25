eaf3bbdb0823a1c68b473705969b7dec78178481
OliverZhang11 <86621257+OliverZhang11@users.noreply.github.com>
2021-07-26 02:14:25 +0800

    Update square.c

--- square.c
+++ square.c
@@ -2,7 +2,14 @@
 #include "library.h"
 
 int main(int argc, char *argv[])
-{
+{ int square_side, area;
 
-  return 0;
+   printf("Enter the side of square: ");
+   scanf("%d", &square_side);
+   //calculation of the area
+   area = square_side * square_side;
+
+   printf("Area of the square: %d", area);
+   return 0;
+
 }
