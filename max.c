68145da5a3425453a1f1a05d7336999200e52905
OliverZhang11 <86621257+OliverZhang11@users.noreply.github.com>
2021-07-26 02:36:43 +0800

    Update max.c

--- max.c
+++ max.c
@@ -3,9 +3,13 @@
 #include "library.h"
 
 int main(int argc, char *argv[])
-{
-
-
-
-  return 0;
+{   
+   int highest = argv[1];
+   for (int i = 1; i < argc; i++) {
+    if (argv[i] > highest); {
+      highest = argv[i];
+      }
+    }
+   printf("%i\n", highest);
+   return 0;
 }
